6e21a34d6c10b1d6982495a4b94f4e76012fd0b5
zdorovilov test <zdorovilovde@mail.ru>
2024-08-03 15:08:11 +0500

    Update DashboardPage

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -1,7 +1,9 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
@@ -17,5 +19,21 @@ public class DashboardPage {
     public DashboardPage() {
         heading.shouldBe(visible);
     }
-    
+    public int getCardBalance(String maskedCardNumber) {
+        var text = cards.findBy(Condition.text(maskedCardNumber)).getText();
+        return extractBalance(text);
+    }
+
+    private int extractBalance(String text) {
+        var start = text.indexOf(balanceStart);
+        var finish = text.indexOf(balanceFinish);
+        var value = text.substring(start + balanceStart.length(),finish);
+        return Integer.parseInt(value);
+    }
+
+
+
+
+
+
 }
