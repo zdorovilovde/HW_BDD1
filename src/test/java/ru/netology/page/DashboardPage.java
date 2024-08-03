256b4f5b5f431bc08395fb7c1934a5f6ccb7b203
zdorovilov test <zdorovilovde@mail.ru>
2024-08-03 14:04:39 +0500

    Add DashboardPage, LoginPage, VerificationPage

---
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -0,0 +1,20 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+    private final SelenideElement heading = $("[data-test-id=dashboard]");
+    private ElementsCollection cards = $$(".list__item div");
+    private final SelenideElement reloadButton = $("[data-test-id='action-reload']");
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
+    }
+}
